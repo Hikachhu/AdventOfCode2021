4e638d6e52c3f62b7c935315c99437b6d7a93121
Hikachhu <florian.morin@gmail.com>
2021-12-08 18:50:34 +0100

    add day3

---
+++ day3/day3.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#define size 12
+int main(){
+	int counter[size]={0},number1=0,number2=0;
+	char direction[size];
+	FILE *fichier=fopen("SourceDay3.txt","r");
+	
+	while(fscanf(fichier,"%s", direction)==1){
+		for(int i=0;i<size;i++){	
+			counter[i]=(direction[i]=='1')?counter[i]+1:counter[i]-1; 
+		}
+	}	
+	for(int i=0;i<size;i++){
+		if(counter[i]>0)number1+=pow(2,size-1-i);
+		else number2+=pow(2,size-1-i);
+	}
+	printf("%d\n",number1*number2);
+	fclose(fichier);
+}
